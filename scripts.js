8dd0d2714899bd64dde4b26ab079fdb7d0b6aa09
Allissonmarques <allissonmarques00@gmail.com>
2025-01-31 11:30:46 -0300

    Projeto cronômetro finalizado.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,46 @@
-let number = 0
-let cron 
-let StopWatch = document.querySelector(".Stopwatch")
+let seconds= 0
+let minutes= 0
+let hours= 0
+let interval
+
+function fullTime(digit){
+    if(digit<10){
+        return("0"+digit)
+    }
+    else{
+        return(digit)
+    }
+}
 
 function startButton(){
-
-    cron = setInterval(function(){
-        number++
-        StopWatch.innerHTML = number
-    },1000)
+    time()
+    interval= setInterval(time, 1000)
 }
 
 function pauseButton(){
-    clearInterval(cron)
+   clearInterval(interval)
 }
 
+function stopButton(){
+    clearInterval(interval)
+    seconds= 0
+    minutes= 0
+    document.getElementById("time").innerHTML= "00:00:00"
+}
 
+function time(){
+    seconds++
+    
+    if(seconds==60){
+        minutes++
+        seconds=0
+        if(minutes==60){
+            hours++
+            minutes=0
+        }
+    }
+    
+    document.getElementById("time").innerHTML=fullTime(hours)+":"+fullTime(minutes)+":"+fullTime(seconds)
+
+    
+}
